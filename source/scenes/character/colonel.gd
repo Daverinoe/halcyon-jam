6acfdf70442a40710e61b0ebd0f31f3e03652cee
Daverinoe <daverinoe@gmail.com>
2022-07-29 22:16:45 +0200

    Basic character controller
    No jump, need to add collisions to tilemaps

---
+++ source/scenes/character/colonel.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+
+var __direction : Vector2 = Vector2.ZERO
+var __velocity : Vector2 = Vector2.ZERO
+var __speed : float = 10.0
+var __gravity : float = 10.0
+
+func _physics_process(delta: float) -> void:
+	
+	if Input.is_action_pressed("move_left"):
+		__direction.x -= 1
+	if Input.is_action_pressed("move_right"):
+		__direction.x += 1
+	
+	__direction.x *= __speed
+	__direction.y += __gravity
+	__velocity = move_and_slide(__direction)
+
+func __reflect() -> void:
+	__direction.x *= -1
