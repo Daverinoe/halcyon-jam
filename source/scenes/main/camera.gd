fb992644048cf49c0d0f87539e6e9afe03c1d920
Daverinoe <daverinoe@gmail.com>
2022-07-31 18:46:32 +0200

    Camera finish!
    Camera works by detecting body enter/exit, and setting flags to disallow travel back until the body has fully exited.

--- source/scenes/main/camera.gd
+++ source/scenes/main/camera.gd
@@ -1,29 +1,52 @@
 extends Node2D
 
-var resolution : Vector2 = Vector2(1024, 768)
+var __resolution : Vector2 = Vector2(1024, 768)
 
+var __can_transition_right : bool = true
+var __can_transition_left : bool = true
+var __can_transition_up : bool = true
+var __can_transition_down : bool = true
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
 
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta: float) -> void:
-#	pass
-
-
 func _on_left_bound_body_entered(body: Node) -> void:
-	self.position.x -= resolution.x
+	if __can_transition_left:
+		self.position.x -= __resolution.x
+		__can_transition_right = false
 
 
 func _on_right_bound_body_entered(body: Node) -> void:
-	self.position.x += resolution.x
+	if __can_transition_right:
+		self.position.x += __resolution.x
+		__can_transition_left = false
 
 
 func _on_top_bound_body_entered(body: Node) -> void:
-	self.position.y -= resolution.y
+	if __can_transition_up:
+		self.position.y -= __resolution.y
+		__can_transition_down = false
 
 
 func _on_bottom_bound_body_entered(body: Node) -> void:
-	self.position.y += resolution.y
+	if __can_transition_down:
+		self.position.y += __resolution.y
+		__can_transition_up = false
+
+
+func _on_left_bound_body_exited(body: Node) -> void:
+	__can_transition_left = true
+
+
+func _on_right_bound_body_exited(body: Node) -> void:
+	__can_transition_right = true
+
+
+func _on_top_bound_body_exited(body: Node) -> void:
+	__can_transition_up = true
+
+
+func _on_bottom_bound_body_exited(body: Node) -> void:
+	__can_transition_down = true
